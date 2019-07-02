61607025ee7a02c53bb65e45e98c5d09974316af
Robin_tty <muellerobin95@gmail.com>
2019-07-02 03:59:33 +0200

    Update example, add comments

--- Assembler/HCS12/Examples/AsmIntro/Sources/main.asm
+++ Assembler/HCS12/Examples/AsmIntro/Sources/main.asm
@@ -36,27 +36,31 @@ Entry:
   
 ; ----- Transfer operations and addressing modes -------------------------------
         
-		LDD con1
-		LDX con2
-		LDY con3
-		MOVW #$2456, v1
-		MOVW #v1, v2
+		LDD con1         ; moves $3210 into D
+		LDX con2         ; moves $0123 into X
+		LDY con3         ; moves $1020 into Y
 		
-		STX 2,-SP
-		STY 2,-SP
-		STAA 1,-SP
-		STAB 1,-SP
-		PULX
-		PULD
-		LDY 2,SP+
+		MOVW $2456, v1   ; moves value at address 0x2456 into v1
+		MOVW #$2456, v1  ; moves $2456 into v1
 		
-		LDX #con1
-		LDY #con2
-		LDAA 4, +X
-		LDAB 1, Y-
-		MOVB #0, v2
-		STD v1+2
-		LDX #$D800
-		LDX $D800
+		MOVW #v1, v2     ; moves address of v1 into v2
+		MOVW v1,v2       ; moves value of v1 into v2 = $2456
+		
+		STX 2,-SP        ; store X on stack (through stack pointer) -> same as PSHX
+		STY 2,-SP        ; store Y on stack (through stack pointer) -> same as PSHY
+		STAA 1,-SP       ; store A on stack (through stack pointer) -> same as PSHA
+		STAB 1,-SP       ; store B on stack (through stack pointer) -> same as PSHB
+		PULX             ; X=$1032 (B & A)
+		PULD             ; D=$1020 (Y)
+		LDY 2,SP+        ; Y=$0123 (X)
+		
+		LDX #con1        ; load address of con1 into X
+		LDY #con2        ; load address of con2 into Y
+		LDAA 4, +X       ; Load value, the address saved in X ($800) + 4 points to, into A = $BA
+		LDAB 1, Y-       ; Load value, the address saved in Y ($808) and decrement by 1 after = $01
+		MOVB #0, v2      ; Load 0 into v2[0]
+		STD v1+2         ; Store D at address of v1 + 2 = v2 -> v2[0] = ba; v2[1] = 01
+		LDX #$D800       ; Load $D800 into X
+		LDX $D800        ; Load value at address $D800
 
 
