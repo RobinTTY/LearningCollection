0b78f693ec91315db0e0d3521f244ca63a058ed8
Robin_tty <muellerobin95@gmail.com>
2019-07-18 23:34:06 +0200

    Add two-fer exercise

---
+++ Exercism/csharp/two-fer/TwoFer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class TwoFer
+{
+    // In order to get the tests running, first you need to make sure the Speak method 
+    // can be called both without any arguments and also by passing one string argument.
+    public static string Speak(string input = "")
+    {
+        return input.Length == 0 ? "One for you, one for me." : $"One for {input}, one for me.";
+    }
+}
