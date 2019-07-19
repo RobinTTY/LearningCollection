7dc9f5386a8c8e66cd5b10218bbdc2082993c2d2
Robin_tty <muellerobin95@gmail.com>
2019-07-19 23:43:01 +0200

    Update TwoFer based d on feedback

--- Exercism/csharp/two-fer/TwoFer.cs
+++ Exercism/csharp/two-fer/TwoFer.cs
@@ -4,8 +4,8 @@ public static class TwoFer
 {
     // In order to get the tests running, first you need to make sure the Speak method 
     // can be called both without any arguments and also by passing one string argument.
-    public static string Speak(string input = "")
+    public static string Speak(string input = "you")
     {
-        return input.Length == 0 ? "One for you, one for me." : $"One for {input}, one for me.";
+        return $"One for {input}, one for me.";
     }
 }
