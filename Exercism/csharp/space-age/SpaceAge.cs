588848e3a29b6e23ff2f1410b93f62431a66488b
Robin_tty <muellerobin95@gmail.com>
2019-07-28 22:40:25 +0200

    Update execexercise to mentor feedback

--- Exercism/csharp/space-age/SpaceAge.cs
+++ Exercism/csharp/space-age/SpaceAge.cs
@@ -2,23 +2,23 @@ using System;
 
 public class SpaceAge
 {
-    private readonly int _age;
+    private readonly double _earthAge;
 
-    public SpaceAge(int seconds) => _age = seconds;
+    public SpaceAge(int seconds) => _earthAge = seconds / (86400 * 365.25);
 
-    public double OnEarth() => _age / (86400 * 365.25);
+    public double OnEarth() => _earthAge;
 
-    public double OnMercury() => OnEarth() / 0.2408467;
+    public double OnMercury() => _earthAge / 0.2408467;
 
-    public double OnVenus() => OnEarth() / 0.61519726;
+    public double OnVenus() => _earthAge / 0.61519726;
 
-    public double OnMars() => OnEarth() / 1.8808158;
+    public double OnMars() => _earthAge / 1.8808158;
 
-    public double OnJupiter() => OnEarth() / 11.862615;
+    public double OnJupiter() => _earthAge / 11.862615;
 
-    public double OnSaturn() => OnEarth() / 29.447498;
+    public double OnSaturn() => _earthAge / 29.447498;
 
-    public double OnUranus() => OnEarth() / 84.016846;
+    public double OnUranus() => _earthAge / 84.016846;
 
-    public double OnNeptune() => OnEarth() / 164.79132;
+    public double OnNeptune() => _earthAge / 164.79132;
 }
