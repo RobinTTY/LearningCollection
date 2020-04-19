5abf36ef180bf9d5d47afd72b134c7953acb3b0b
Robin_tty <muellerobin95@gmail.com>
2020-04-19 18:54:27 +0200

    More examples

---
+++ Python/07_LoopControl.py
@@ -0,0 +1,11 @@
+limit = 25
+numbers = []
+while len(numbers) < 5:
+    for i in range(limit):
+        if i % 3 != 0:
+            continue
+        else:
+            numbers.append(i)
+ 
+print(len(numbers))
+print(numbers)
