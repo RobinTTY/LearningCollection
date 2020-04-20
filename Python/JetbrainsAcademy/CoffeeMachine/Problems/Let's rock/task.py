f34d036e4a0d42cbc72841dcda0f8409088e2447
RobinTTY <muellerobin95@gmail.com>
2020-04-20 04:40:26 +0200

    some more progress

---
+++ Python/JetbrainsAcademy/CoffeeMachine/Problems/Let's rock/task.py
@@ -0,0 +1,11 @@
+# start a RockBand here
+class RockBand:
+    genre = "rock"
+    n_members = 4
+    famous_songs = list()
+
+
+sabaton = RockBand()
+print(sabaton.genre)
+print(sabaton.n_members)
+print(sabaton.famous_songs)
