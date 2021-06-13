2046c0a774d1036a9920de2fca8aac8732004874
RobinTTY <muellerobin95@gmail.com>
2021-06-13 16:07:25 +0200

    Add project template

---
+++ Databases/MongoDB/M220N MongoDB for .NET Developers/src/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDBUniversityM220N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = File.ReadAllText("connectionString.txt");
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("sample_mflix");
+            var collection = database.GetCollection<BsonDocument>("movies");
+            var result = collection.Find("{title:'The Princess Bride'}").FirstOrDefault();
+            Console.WriteLine(result);
+        }
+    }
+}
