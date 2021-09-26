cb273ce9875119a283c92a766cf4d93ae9632fb7
RobinTTY <muellerobin95@gmail.com>
2021-09-26 16:01:12 +0200

    Add visual bump when an item is added

--- Web/React/in depth guide/07_Food_Order_App/src/components/Layout/HeaderCartButton.js
+++ Web/React/in depth guide/07_Food_Order_App/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartContext = useContext(CartContext);
 
   const numberOfCartItems = cartContext.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  // handle the button bump effect when an item is added to the cart
+  useEffect(() => {
+    if (cartContext.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    // cleanup function through return
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartContext.items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
