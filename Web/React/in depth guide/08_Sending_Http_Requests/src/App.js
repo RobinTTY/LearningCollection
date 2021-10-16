e822469e33d14688ea078ab12200b7366c0fa565
RobinTTY <muellerobin95@gmail.com>
2021-10-16 16:44:39 +0200

    Add load indicator and fallback

--- Web/React/in depth guide/08_Sending_Http_Requests/src/App.js
+++ Web/React/in depth guide/08_Sending_Http_Requests/src/App.js
@@ -5,8 +5,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films");
     const data = await response.json();
     const transformedMovies = data.results.map((movie) => {
@@ -18,6 +20,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -26,7 +29,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <h1>No movies to show</h1>}
+        {isLoading && <h1>Loading...</h1>}
       </section>
     </React.Fragment>
   );
