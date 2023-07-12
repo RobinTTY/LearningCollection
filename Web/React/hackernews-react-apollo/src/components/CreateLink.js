434c89cef05097a03390e69e84b35f2842937cb4
RobinTTY <muellerobin95@gmail.com>
2023-07-13 01:55:02 +0200

    Add routing

--- Web/React/hackernews-react-apollo/src/components/CreateLink.js
+++ Web/React/hackernews-react-apollo/src/components/CreateLink.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useMutation, gql } from "@apollo/client";
+import { useNavigate } from 'react-router-dom';
 
 const CREATE_LINK_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
@@ -13,6 +14,9 @@ const CREATE_LINK_MUTATION = gql`
 `;
 
 const CreateLink = () => {
+  const navigate = useNavigate();
+
+  // Saves the current state of the form
   const [formState, setFormState] = useState({
     description: "",
     url: "",
@@ -22,14 +26,19 @@ const CreateLink = () => {
   // the mutation, that’s what createLink is. We’re now free to call the function whenever we need to.
   const [createLink] = useMutation(CREATE_LINK_MUTATION, {
     variables: {
+      // description of a news article
       description: formState.description,
+      // link for the news article
       url: formState.url,
     },
+    // When the mutation is complete, we navigate the user back to the news feed
+    onCompleted: () => navigate('/')
   });
 
   return (
     <div>
       <form
+        // On submission the mutation is called with the current state of the form
         onSubmit={(e) => {
           e.preventDefault();
           createLink();
@@ -38,7 +47,9 @@ const CreateLink = () => {
         <div className="flex flex-column mt3">
           <input
             className="mb2"
+            // The first input is bound to the formState.description state
             value={formState.description}
+            // When the input changes, we update the formState.description state
             onChange={(e) =>
               setFormState({
                 ...formState,
@@ -50,7 +61,9 @@ const CreateLink = () => {
           />
           <input
             className="mb2"
+            // The second input is bound to the formState.url state
             value={formState.url}
+            // When the input changes, we update the formState.url state
             onChange={(e) =>
               setFormState({
                 ...formState,
--- Web/React/hackernews-react-apollo/src/index.js
+++ Web/React/hackernews-react-apollo/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.css";
+import { BrowserRouter } from 'react-router-dom';
 import App from "./components/App";
+import "./styles/index.css";
 
-// 1 Necessary dependencies
+// 1. Necessary dependencies
 import {
   ApolloProvider,
   ApolloClient,
@@ -11,21 +12,23 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 
-// 2 The GraphQL server endpoint
+// 2. The GraphQL server endpoint
 const httpLink = createHttpLink({
   uri: "http://localhost:4000",
 });
 
-// 3 Instantiate ApolloClient
+// 3. Instantiate ApolloClient
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
 });
 
-// 4 Render the root component wrapped in ApolloProvider
+// 4. Render the root component wrapped in ApolloProvider
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById("root")
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
