a6af2e9b34683b5357eeeeea3dbdbdbedbbe27d4
RobinTTY <muellerobin95@gmail.com>
2023-10-08 19:56:03 +0200

    Add graphql server setup

--- learning-projects/csharp/graphql-hotchocolate/src/Program.cs
+++ learning-projects/csharp/graphql-hotchocolate/src/Program.cs
@@ -1,12 +1,15 @@
+using GraphQLHotChocolate;
 using GraphQLHotChocolate.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=conferences.db"));
+builder.Services
+    .AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=conferences.db"))
+    .AddGraphQLServer()
+    .AddQueryType<Query>();
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapGraphQL();
 app.Run();
 
 // Database Migration: https://learn.microsoft.com/en-us/ef/core/managing-schemas/migrations/?tabs=dotnet-core-cli
@@ -14,3 +17,11 @@ app.Run();
 // removed, and database schemas need to be changed accordingly to be kept in sync with the application. The
 // migrations feature in EF Core provides a way to incrementally update the database schema to keep it in sync with
 // the application's data model while preserving existing data in the database.
+
+// Important Commands:
+// Add-Migration <migration-name>
+// => Files are added to your project under the Migrations directory:
+// => MyContextModelSnapshot.cs - A snapshot of your current model. Used to determine what changed when adding the next migration
+// => AddCreatedTimestamp.cs - The main migrations file. Contains the operations necessary to apply the migration (in Up) and to revert it (in Down)
+// Update-Database
+// => Creates your database and schema from the migration
