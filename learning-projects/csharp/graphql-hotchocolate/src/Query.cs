7e3316f95b9827fc7494add53fcfebda39f898b0
RobinTTY <muellerobin95@gmail.com>
2023-10-10 17:44:56 +0200

    Align folder naming across docs

---
+++ learning-projects/csharp/graphql-hotchocolate/src/Query.cs
@@ -0,0 +1,11 @@
+﻿using GraphQLHotChocolate.Data;
+using HotChocolate;
+
+namespace GraphQLHotChocolate
+{
+    public class Query
+    {
+        public IQueryable<Speaker> GetSpeakers([Service] ApplicationDbContext context) =>
+            context.Speakers;
+    }
+}
