124cbb831fb3b2dbe9cc7af711320f7d94990617
RobinTTY <muellerobin95@gmail.com>
2023-10-13 03:17:32 +0200

    Add a basic controller

---
+++ learning-projects/csharp/aspnet-efcore7/Controllers/CharacterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_efcore7;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CharacterController : ControllerBase
+{
+  private static Character knight = new Character();
+
+  [HttpGet]
+  public IActionResult Get()
+  {
+    return Ok(knight);
+  }
+}
---
+++ learning-projects/csharp/aspnet-efcore7/Models/RpgClass.cs
@@ -0,0 +1,8 @@
+﻿namespace aspnet_efcore7;
+
+public enum RpgClass
+{
+  Knight = 1,
+  Mage = 2,
+  Cleric = 3
+}
