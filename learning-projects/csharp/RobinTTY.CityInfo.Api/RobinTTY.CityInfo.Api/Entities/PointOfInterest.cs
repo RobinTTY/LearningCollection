9ac8f17cbc74a8d3c3aeb8c188b8c41add89e76b
RobinTTY <muellerobin95@gmail.com>
2024-08-26 01:41:25 +0200

    Add basic database entities

---
+++ learning-projects/csharp/RobinTTY.CityInfo.Api/RobinTTY.CityInfo.Api/Entities/PointOfInterest.cs
@@ -0,0 +1,16 @@
+﻿namespace RobinTTY.CityInfo.Api.Entities;
+
+public class PointOfInterest
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+
+    public City? City { get; set; }
+    public int CityId { get; set; }
+
+    public PointOfInterest(string name) => Name = name;
+}
